1e01e738acd4b960557a8db89108c2178013e55d
jameschen56 <jameschen56@gmail.com>
2022-03-15 14:33:45 -0700

    completed env file

---
+++ frontend/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import App from './App';
+import { ModalProvider } from './context/Modal';
+import { restoreCSRF, csrfFetch } from './store/csrf'
+import configureStore from './store';
+import * as sessionActions from './store/session.js'
+import './index.css';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <ReduxProvider store={store}>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
+    </ReduxProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
