807c1e19174acf670ba4fec6ca70a58efdef5062
jameschen56 <jameschen56@gmail.com>
2022-03-22 16:55:17 -0700

    added splash image to the homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -22,38 +22,41 @@ const Home = () => {
   }, [events]);
 
   return (
-    <div>
-      <div className="events-main">
-        <div className="popular-logo">
-          <h3>Popular in United States</h3>
-        </div>
-        <div className="events-content">
-          {eventList.length &&
-            eventList.map((event) => (
-              <Link
-                key={`single_event_link_${event?.id}`}
-                to={`/events/${event?.id}`}
-              >
-                <div className="all-events-container">
-                  <div className="event_images">
-                    <img
-                      height={250}
-                      alt={event?.title}
-                      src={
-                        event?.imageUrl
-                          ? event?.imageUrl
-                          : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
-                      }
-                    />
+    <>
+      <img id="splash-image" src="https://images.unsplash.com/photo-1578946956088-940c3b502864?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2223&q=80" alt=""></img>
+      <div>
+        <div className="events-main">
+          <div className="popular-logo">
+            <h3>Popular in United States</h3>
+          </div>
+          <div className="events-content">
+            {eventList.length &&
+              eventList.map((event) => (
+                <Link
+                  key={`single_event_link_${event?.id}`}
+                  to={`/events/${event?.id}`}
+                >
+                  <div className="all-events-container">
+                    <div className="event_images">
+                      <img
+                        height={250}
+                        alt={event?.title}
+                        src={
+                          event?.imageUrl
+                            ? event?.imageUrl
+                            : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
+                        }
+                      />
+                    </div>
+                    <div className="event_title">{event?.title}</div>
                   </div>
-                  <div className="event_title">{event?.title}</div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
+          </div>
         </div>
+        <MyFooter />
       </div>
-      <MyFooter />
-    </div>
+    </>
   );
 };
 
