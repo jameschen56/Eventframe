84d6edf46b052cce93b41348fd1d44f6622a81c0
jameschen56 <jameschen56@gmail.com>
2022-03-22 17:36:48 -0700

    added banner to the homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -24,6 +24,10 @@ const Home = () => {
   return (
     <>
       <img id="splash-image" src="https://images.unsplash.com/photo-1578946956088-940c3b502864?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2223&q=80" alt=""></img>
+      <div className='adsBanner'>
+            <h1>Re-open confidently with Eventframe’s COVID-19 Safety Playbook</h1>
+            <p>We do not partnered with risk management and health experts to empower event creators to thoughtfully consider potential safety and security risks at your event. </p>
+      </div>
       <div>
         <div className="events-main">
           <div className="popular-logo">
